8fbbf88601f078ef3da6a9e0c1c1cfbd0ec01445
Kawsar Ahmed <prokawsar@gmail.com>
2020-10-24 22:17:20 +0600

    access to user

---
+++ js-fe57e35/chunk-97fddd3e.js
@@ -0,0 +1,2 @@
+var K_ENV="prod",K_ENV_DEV="dev",K_ENV_TEST="test",K_ENV_PROD="prod";import{s as baseModel}from"./chunk-5d88ab61.js";var Model=new class extends baseModel{constructor(){super(),this.apiEndpoint="/case_histories"}};export{Model as a};
+//# sourceMappingURL=chunk-97fddd3e.js.map
